d3c6525b702ff9a37f08c2f65f1552df47b9e30a
MayrusNanami <mayrariverabrave@gmail.com>
2024-09-23 10:03:53 -0500

    app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+# Usar CSS para hacer que la imagen cubra toda la pantalla
+st.markdown(
+    """
+    <style>
+    body {
+        background-image: url('images/Iniciar Sesion.png');
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Puedes agregar otros elementos si lo necesitas, pero la imagen ya debería estar en pantalla completa
+st.title(" ")
+st.text(" ")
